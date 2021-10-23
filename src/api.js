36a215b63c00fd469d65a5e21e6d6dd2176f4724
metrupki <67233660+metrupki@users.noreply.github.com>
2021-10-23 23:42:03 +0200

    added https to api call

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-const base_url = 'http://openlibrary.org';
+const base_url = 'https://openlibrary.org';
 
 export const subjects = (subject, limit, offset) => `${base_url}/subjects/${subject}.json?limit=${limit}&offset=${offset}`;
 
 export const bookDetails = (key) => `${base_url}${key}.json`;
 
-export const getCover = (cover_id, size) => `http://covers.openlibrary.org/b/ID/${cover_id.toString()}-${size}.jpg`;
+export const getCover = (cover_id, size) => `https://covers.openlibrary.org/b/ID/${cover_id.toString()}-${size}.jpg`;
